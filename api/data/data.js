272e4a18e80a516e4c329917d52b50125330bc07
Laoset <xlaospt2@gmail.com>
2023-05-19 19:24:14 -0300

    re

---
+++ api/data/data.js
@@ -0,0 +1,132 @@
+export let data = {
+  users: [
+    {
+      id: 1,
+      first_name: "admin",
+      last_name: "admin",
+      email: "admin@admin.com",
+      password: "admin",
+    },
+  ],
+  products: [
+    {
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/0/09/TANGO_MUNDIAL_78.jpg",
+      id: 2,
+      name: "Pelota",
+      price: 149.99,
+      category: "Deporte",
+    },
+    {
+      id: 3,
+      image:
+        "https://resource.logitech.com/w_386,ar_1.0,c_limit,f_auto,q_auto,dpr_2.0/d_transparent.gif/content/dam/logitech/en/products/mice/m317-wireless-mouse/gallery/m317-white-gallery-1.png?v=1",
+      name: "Mouse",
+      price: 350,
+      category: "Electronicos",
+    },
+    {
+      id: 4,
+      image: "https://infotec.com.ec/wp-content/uploads/2021/02/XTK-130_2.jpg",
+      name: "Teclado",
+      price: 250,
+      category: "Electronicos",
+    },
+    {
+      id: 5,
+      image:
+        "https://www.digitalavmagazine.com/wp-content/uploads/2020/08/Philips-279C9.jpg",
+      name: "Monitor",
+      price: 1000,
+      category: "Electronicos",
+    },
+    {
+      id: 6,
+      image:
+        "https://www.nvidia.com/content/dam/en-zz/Solutions/geforce/ampere/rtx-3090/geforce-rtx-3090-shop-600-p.png",
+      name: "Nvidia RTX 3090",
+      price: 1500,
+      category: "Gaming",
+    },
+    {
+      id: 7,
+      image:
+        "https://images.fravega.com/f300/a943c6a63392fc065341cef1198afe2f.jpg.webp",
+      name: "Estante flotante",
+      price: 100,
+      category: "Hogar",
+    },
+    {
+      id: 8,
+      image:
+        "https://www.publimania.com.ar/wp-content/uploads/2021/09/01.009.jpg",
+      name: "Botella deportiva",
+      price: 75,
+      category: "Deporte",
+    },
+    {
+      id: 9,
+      image:
+        "https://safetystoremx.vteximg.com.br/arquivos/ids/156347-460-460/guantes-anticorte-dermacare.jpg",
+      name: "Guantes",
+      price: 35,
+      category: "Indumentaria",
+    },
+    {
+      id: 10,
+      image:
+        "https://nuro.com.ar/wp-content/uploads/2021/04/CAMPERA-BABOLAT.IMG01.jpg.webp",
+      name: "Campera",
+      price: 20,
+      category: "Indumentaria",
+    },
+    {
+      id: 11,
+      image:
+        "https://media.lacapital.com.ar/p/355e07f7b2ab856d5b64e9636facd3d2/adjuntos/205/imagenes/030/697/0030697639/642x0/smart/parlante-bluetoth-3jpg.jpg",
+      name: "Parlante",
+      price: 300,
+      category: "Electronicos",
+    },
+    {
+      id: 12,
+      image:
+        "https://es.thermaltake.com/media/catalog/product/cache/023a745bb14092c479b288481f91a1bd/db/imgs/pdt/angle/CA-1K7-00M1NN-00_bf5dbaf8595e46f9969d74ef252f9089.jpg",
+      name: "Gabinete Tt",
+      price: 200,
+      category: "Gaming",
+    },
+    {
+      id: 13,
+      image:
+        "https://fullh4rd.com.ar/img/productos/Pics_Prod/fuente-600w-thermaltake-smart-white-80-plus-0.jpg",
+      name: "Fuente 600w 80plus White",
+      price: 400,
+      category: "Gaming",
+    },
+    {
+      id: 14,
+      image:
+        "https://www.tripstore.com.ar/media/catalog/product/cache/4769e4d9f3516e60f2b4303f8e5014a8/C/J/CJ0636-500_0.jpg",
+      name: "Zapatilla",
+      price: 250,
+      category: "Indumentaria",
+    },
+    {
+      id: 15,
+      image: "https://files.coolermaster.com/og-image/mp720-1200x630.jpg",
+      name: "MousePad",
+      price: 60,
+      category: "Gaming",
+    },
+    {
+      id: 16,
+      image:
+        "https://surtiloza.mx/wp-content/uploads/2017/02/rallador-de-queso-4-lados.jpg",
+      name: "Rayador",
+      price: 10,
+      category: "Hogar",
+    },
+  ],
+};
+export default data;
---
+++ api/src/index.js
@@ -0,0 +1,64 @@
+import data from "../data/data";
+const express = require("express");
+const app = express();
+var cors = require("cors");
+app.use(cors());
+//depedencia para post
+app.use(express.json());
+//REQ
+app.get("/", (req, res) => {
+  res.send("Bienvenido a la API ");
+});
+app.get("/api/usuarios", (req, res) => {
+  res.send(data);
+});
+app.get("/api/productos", (req, res) => {
+  res.send(data.products);
+});
+
+app.get("/api/usuarios/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const usuarios = data.users.find((usuario) => usuario.id === id);
+  if (usuarios) {
+    res.json(usuarios);
+  } else {
+    res.status(404).end();
+  }
+});
+app.get("/api/productos/:id", (req, res) => {
+  const idProduct = Number(req.params.id);
+  console.log(idProduct);
+  const productos = data.products.find((producto) => producto.id === idProduct);
+  res.json(productos);
+});
+app.get("/api/productos/category/:category", (req, res) => {
+  const categoryProduct = req.params.category;
+  console.log(categoryProduct);
+  const productos = data.products.filter(
+    (producto) => producto.category === categoryProduct
+  );
+  res.json(productos);
+});
+//POST DE USUARIOS
+app.post("/api/usuarios", (req, res) => {
+  const info = req.body;
+
+  const ids = data.users.map((dato) => dato.id);
+  const maxId = Math.max(...ids);
+  const newUsuario = {
+    id: maxId + 1,
+    first_name: info.first_name,
+    last_name: info.last_name,
+    email: info.email,
+    password: info.password,
+  };
+  data.users = [...data.users, newUsuario];
+  res.json(newUsuario);
+});
+
+const PORT = 8080;
+app.listen(PORT, () =>
+  console.log(`Servidor en funcionamiento en el puerto ${PORT}`)
+);
+
+module.exports = app;
